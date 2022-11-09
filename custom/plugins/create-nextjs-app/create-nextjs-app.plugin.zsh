b6cb7da3afbf5c70b556ca805bfe7cd126ae87cf
Samet Tutkun <tutkun@users.noreply.github.com>
2022-11-09 20:20:37 +0300

    .

--- custom/plugins/create-nextjs-app/create-nextjs-app.plugin.zsh
+++ custom/plugins/create-nextjs-app/create-nextjs-app.plugin.zsh
@@ -4,9 +4,10 @@
 # GitHub Projesi oluşturma yardımı
 function create-nextjs-app(){
     # CURR_DIR=$PWD
-    DIR=$HOME/Developer/sites
+    DIR="${HOME}/Developer/sites"
+    GITHUB="https://github.com/tutkun/${1}"
 
-    PROJECT_DIR=$DIR/$1
+    PROJECT_DIR="${DIR}/${1}"
 
     if [ -d "$PROJECT_DIR" ]; then
         echo "\n\e[31mUYARI:\e[m" "\t\e[31m$1\e[m \e[32mdizini zaten mevcut. Bu işlem dizindeki verileri korumak için durduruldu.\e[m\n" "\t\e[34m$DIR/\e[31m$1\e[m\e[m\n"
@@ -14,19 +15,22 @@ function create-nextjs-app(){
         # NextJS projesi çekiliyor
         # npx create-next-app $PROJECT_DIR --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
         git clone https://github.com/tutkun/nextjs-webapp-template $PROJECT_DIR
-        rm -rf $PROJECT_DIR/.git/
+        # rm -rf $PROJECT_DIR/.git/
         
         # projenin kurulacağı dizine geç
         cd $PROJECT_DIR
-        echo "$(PROJECT_DIR) dizinine geçiş yapıldı..."
+        echo "${PROJECT_DIR} dizinine geçiş yapıldı..."
         
         # git ile projeye dosyaları ekle
-        git init
+        # git init
         git add .
         git commit -m "Uzay yolculuğu başladı... 🚀"
-        git branch -M master
-        git remote add origin https://github.com/tutkun/$1.git
+        # git branch -M master
+        # git remote add origin "${GITHUB}.git"
+        git remote set-url origin "${GITHUB}"
         git push -u origin master
+
+        echo "${GITHUB} Reposu oluşturuldu 🚀"
 
         # bir önceki dizine geri dön
         echo "Bir önceki dizin olan \"$(-)\" dizine geçiş yapılıyor..."
