2cad37a6efe9f5bfdc53ef44ba086f0e11039585
Samet Tutkun <tutkun@users.noreply.github.com>
2022-11-13 18:16:13 +0300

    update

--- custom/plugins/command-not-found/command-not-found.plugin.zsh
+++ custom/plugins/command-not-found/command-not-found.plugin.zsh
@@ -28,7 +28,7 @@ if [[ -x /usr/lib/command-not-found || -x /usr/share/command-not-found/command-n
       /usr/share/command-not-found/command-not-found -- "$1"
       return $?
     else
-      printf "zsh: komut bulunamadı: %s\n" "$1" >&2
+      printf "zsh: komut bulunamadi: %s\n" "$1" >&2
       return 127
     fi
   }
@@ -42,7 +42,7 @@ if [[ -x /usr/libexec/pk-command-not-found ]]; then
       return $?
     fi
     
-    printf "zsh: komut bulunamadı: %s\n" "$1" >&2
+    printf "zsh: komut bulunamadi: %s\n" "$1" >&2
     return 127
   }
 fi
