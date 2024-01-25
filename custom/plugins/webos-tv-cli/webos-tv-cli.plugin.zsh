fe74774955c3195859081391b64de16a9af42541
Samet Tutkun <tutkun@users.noreply.github.com>
2024-01-25 14:25:00 +0100

    add: update for webos-tv-cli ✨

--- custom/plugins/webos-tv-cli/webos-tv-cli.plugin.zsh
+++ custom/plugins/webos-tv-cli/webos-tv-cli.plugin.zsh
@@ -6,6 +6,7 @@ export LG_WEBOS_TV_SDK_HOME="${HOME}/webOS_TV_SDK"
 
 if [ -d "$LG_WEBOS_TV_SDK_HOME/CLI/bin" ]; then
   # echo "\n\e[31mUYARI:\e[m" "\t\e[31m$1\e[m \e[32mdizini zaten mevcut. Bu işlem dizindeki verileri korumak için durduruldu.\e[m\n" "\t\e[34m$DIR/\e[31m$1\e[m\e[m\n"
+  
   # Setting the WEBOS_CLI_TV variable to the bin directory of CLI
   export WEBOS_CLI_TV="$LG_WEBOS_TV_SDK_HOME/CLI/bin"
   # Adding the bin directory of CLI to the PATH variable
