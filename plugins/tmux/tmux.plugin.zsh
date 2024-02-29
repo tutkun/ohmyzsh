7c41d4aa4b69c5ed9a93eece4642c934c15caf96
Jonathan De Troye <detroyejr@outlook.com>
2024-02-29 13:05:54 -0500

    fix(tmux): do not pass empty flags to aliases (#12232)
    This enables compatibility with tmux >3.3a
    
    Co-authored-by: Carlo Sala <carlosalag@protonmail.com>

--- plugins/tmux/tmux.plugin.zsh
+++ plugins/tmux/tmux.plugin.zsh
@@ -52,27 +52,13 @@ fi
 
 # ALIASES
 function _build_tmux_alias {
-  setopt localoptions no_rc_expand_param
   eval "function $1 {
-    if [[ -z \$1 ]] || [[ \${1:0:1} == '-' ]]; then
+    if [[ -z \$1 ]] || [[ \${1::1} == '-' ]]; then
       tmux $2 \"\$@\"
     else
       tmux $2 $3 \"\$@\"
     fi
   }"
-
-  local f s
-  f="_omz_tmux_alias_${1}"
-  s=(${(z)2})
-
-  eval "function ${f}() {
-    shift words;
-    words=(tmux ${@:2} \$words);
-    ((CURRENT+=${#s[@]}+1))
-    _tmux
-  }"
-
-  compdef "$f" "$1"
 }
 
 alias tksv='tmux kill-server'
