b754a8f435049ddb877b558b64c75c463c22604a
Robert Crews <rcrews@mac.com>
2024-03-03 04:04:19 -0800

    feat(python): add auto venv activation (#12248)
    Co-authored-by: Carlo Sala <carlosalag@protonmail.com>

--- plugins/python/python.plugin.zsh
+++ plugins/python/python.plugin.zsh
@@ -47,29 +47,12 @@ alias pygrep='grep -nr --include="*.py"'
 alias pyserver="python3 -m http.server"
 
 
-## venv settings
+## venv utilities
 : ${PYTHON_VENV_NAME:=venv}
 
-# Array of possible virtual environment names to look for, in order
-# -U for removing duplicates
-typeset -gaU PYTHON_VENV_NAMES
-[[ -n "$PYTHON_VENV_NAMES" ]] || PYTHON_VENV_NAMES=($PYTHON_VENV_NAME venv .venv)
-
 # Activate a the python virtual environment specified.
-# If none specified, use the first existing in $PYTHON_VENV_NAMES.
+# If none specified, use $PYTHON_VENV_NAME, else 'venv'.
 function vrun() {
-  if [[ -z "$1" ]]; then
-    local name
-    for name in $PYTHON_VENV_NAMES; do
-      local venvpath="${name:P}"
-      if [[ -d "$venvpath" ]]; then
-        vrun "$name"
-        return $?
-      fi
-    done
-    echo >&2 "Error: no virtual environment found in current directory"
-  fi
-
   local name="${1:-$PYTHON_VENV_NAME}"
   local venvpath="${name:P}"
 
@@ -88,7 +71,7 @@ function vrun() {
 }
 
 # Create a new virtual environment using the specified name.
-# If none specified, use $PYTHON_VENV_NAME
+# If none specfied, use $PYTHON_VENV_NAME
 function mkv() {
   local name="${1:-$PYTHON_VENV_NAME}"
   local venvpath="${name:P}"
@@ -100,21 +83,11 @@ function mkv() {
 
 if [[ "$PYTHON_AUTO_VRUN" == "true" ]]; then
   # Automatically activate venv when changing dir
-  function auto_vrun() {
-    # deactivate if we're on a different dir than VIRTUAL_ENV states
-    # we don't deactivate subdirectories!
-    if (( $+functions[deactivate] )) && [[ $PWD != ${VIRTUAL_ENV:h}* ]]; then
-      deactivate > /dev/null 2>&1
-    fi
-
-    if [[ $PWD != ${VIRTUAL_ENV:h} ]]; then
-      local file
-      for file in "${^PYTHON_VENV_NAMES[@]}"/bin/activate(N.); do
-        # make sure we're not in a venv already
-        (( $+functions[deactivate] )) && deactivate > /dev/null 2>&1
-        source $file > /dev/null 2>&1
-        break
-      done
+  auto_vrun() {
+    if [[ -f "${PYTHON_VENV_NAME}/bin/activate" ]]; then
+      source "${PYTHON_VENV_NAME}/bin/activate" > /dev/null 2>&1
+    else
+      (( $+functions[deactivate] )) && deactivate > /dev/null 2>&1
     fi
   }
   add-zsh-hook chpwd auto_vrun
