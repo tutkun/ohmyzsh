855376b17aa5a3eef4389314e0e4666e26f51332
Zikoeng Xi <github@zikoengxi.com>
2024-03-26 16:54:04 +0800

    fix(poetry-env): activate only if env exists (#12301)

--- plugins/poetry-env/poetry-env.plugin.zsh
+++ plugins/poetry-env/poetry-env.plugin.zsh
@@ -5,20 +5,14 @@ _togglePoetryShell() {
     in_poetry_dir=1
   fi
 
-  # Deactivate the current environment if moving out of a Poetry directory or into a different Poetry directory
-  if [[ $poetry_active -eq 1 ]] && { [[ $in_poetry_dir -eq 0 ]] || [[ "$PWD" != "$poetry_dir"* ]]; }; then
-    export poetry_active=0
-    unset poetry_dir
-    (( $+functions[deactivate] )) && deactivate
-  fi
-
-  # Activate the environment if in a Poetry directory and no environment is currently active
-  if [[ $in_poetry_dir -eq 1 ]] && [[ $poetry_active -ne 1 ]]; then
-    venv_dir=$(poetry env info --path 2>/dev/null)
-    if [[ -n "$venv_dir" ]]; then
-      export poetry_active=1
-      export poetry_dir="$PWD"
-      source "${venv_dir}/bin/activate"
+  # activate the environment if pyproject.toml exists
+  if [[ "$poetry_active" != 1 ]]; then
+    if [[ -f "$PWD/pyproject.toml" ]]; then
+      if grep -q 'tool.poetry' "$PWD/pyproject.toml" && venv_dir=$(poetry env info --path); then
+        export poetry_active=1
+        export poetry_dir="$PWD"
+        source "${venv_dir}/bin/activate"
+      fi
     fi
   fi
 }
