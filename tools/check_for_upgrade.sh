1cabccdb7cd93233a2e3fb4a9eb7562ac8d190e9
Marc Cornellà <marc@mcornella.com>
2024-04-18 07:33:13 +0200

    fix(updater): abort update if `$ZSH` is not a git repository
    Fixes #12298

--- tools/check_for_upgrade.sh
+++ tools/check_for_upgrade.sh
@@ -27,8 +27,8 @@ zstyle -s ':omz:update' mode update_mode || {
 # - $ZSH is not a git repository
 if [[ "$update_mode" = disabled ]] \
    || [[ ! -w "$ZSH" || ! -O "$ZSH" ]] \
-   || [[ ! -t 1 && ${POWERLEVEL9K_INSTANT_PROMPT:-off} == off ]] \
-   || ! command git --version 2>&1 >/dev/null \
+   || [[ ! -t 1 ]] \
+   || ! command git --version 2>&1 >/dev/null
    || (builtin cd -q "$ZSH"; ! command git rev-parse --is-inside-work-tree &>/dev/null); then
   unset update_mode
   return
