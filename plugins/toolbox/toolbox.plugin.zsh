046d14297f27c97257454274f99cbc369668b0dc
SpeakinTelnet <105018871+SpeakinTelnet@users.noreply.github.com>
2024-08-02 10:37:00 -0400

    feat(toolbox): add function to show toolbox name (#12594)
    Co-authored-by: Carlo Sala <carlosalag@protonmail.com>

--- plugins/toolbox/toolbox.plugin.zsh
+++ plugins/toolbox/toolbox.plugin.zsh
@@ -3,13 +3,7 @@ function toolbox_prompt_info() {
 }
 
 function toolbox_prompt_name() {
-  [[ -f /run/.containerenv ]] || return
-
-  # This command reads the /run/.containerenv file line by line and extracts the
-  # container name from it by looking for the `name="..."` line, and uses -F\" to
-  # split the line by double quotes. Then all % characters are replaced with %%
-  # to escape them for the prompt.
-  awk -F\" '/name/ { gsub(/%/, "%%", $2); print $2 }' /run/.containerenv
+  [[ -f /run/.containerenv ]] && cat /run/.containerenv | awk -F\" '/name/ { print$2 }'
 }
 
 alias tbe="toolbox enter"
