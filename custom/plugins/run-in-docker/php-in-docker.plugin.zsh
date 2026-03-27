adb050bf04d59c7b753f950de2f46aa3e5451088
Samet <tutkun@users.noreply.github.com>
2026-03-27 22:58:44 +0100

    update(*-in-docker): uygulamalar eklendi ✨

--- custom/plugins/run-in-docker/php-in-docker.plugin.zsh
+++ custom/plugins/run-in-docker/php-in-docker.plugin.zsh
@@ -4,6 +4,12 @@
 # php-in-docker.plugin.zsh
 # ============================================
 
+# core plugin yüklü mü kontrol et
+if ! typeset -f _run_in_docker >/dev/null; then
+  echo "[run-in-docker] ERROR: core plugin not loaded"
+  return 1
+fi
+
 php() {
   _run_in_docker php php "$@"
 }
